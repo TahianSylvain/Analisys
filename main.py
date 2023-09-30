5b1bbf6f75f5b7a0a95e24834d2e0cdf20685497
TahianSylvain <ranjalahyandrytahiansylvain@gmail.com>
2023-09-30 08:52:05 +0300

    Amélioration3.0 du code: good interactivity

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3.9
-a, b, c = "cat", "dog", "chess"
+a = str(input('Enter a word: '))
+b = str(input('Enter another word: '))
+
 ls = lambda a, b: len(b) if not a else len(a) if not b else min(ls(a[1:], b[1:])+(a[0]!=b[0]), ls(a[1:], b)+1, ls(a, b[1:])+1)
-print(ls(a, c))
+
+exit(f'Leveinshtein\'distance is: {ls(a, b)}')
